18c86b472918351e4ef73073a042aca4a08b44fd
Lex Lacson <lex.lacson.dev@gmail.com>
2016-06-27 21:04:18 -0700

    use it in app with stub data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function () {
+    return {
+      stations: [{
+        name: "station 1"
+      },
+      {
+        name: "station 2"
+      },
+      {
+        name: "station 3"
+      },
+      {
+        name: "station 4"
+      }]
+    };
+  }
 });
