4b8fff5a89024b710cbf022435952830e2e3d7f5
Lex Lacson <lex.lacson.dev@gmail.com>
2016-06-30 20:50:01 -0700

    load all stations

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,17 @@
 import Ember from 'ember';
+import ajax from 'ic-ajax';
+import xmlToJson from 'barticle/utils/xmlToJson';
 
 export default Ember.Route.extend({
   model: function () {
-    return {
-      stations: [{
-        name: "station 1"
-      },
-      {
-        name: "station 2"
-      },
-      {
-        name: "station 3"
-      },
-      {
-        name: "station 4"
-      }]
-    };
+    return Ember.RSVP.hash({
+      stations: ajax('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V')
+        .then(function (response) {
+          var json = xmlToJson(response);
+          Ember.Logger.log(json);
+          var stations = json.root.stations.station;
+          return stations;
+        })
+      });
   }
 });
