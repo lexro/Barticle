98fe87036d99e20310aebc7ca5361aa0d1088f05
Lex Lacson <lex.lacson.dev@gmail.com>
2016-07-05 22:27:30 -0700

    calculate and show end stations

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,9 +8,21 @@ export default Ember.Route.extend({
       stations: ajax('https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V')
         .then(function (response) {
           var json = xmlToJson(response);
-          Ember.Logger.log(json);
           var stations = json.root.stations.station;
+
+          Ember.Logger.log(json);
+
           return stations;
+        }),
+
+      routes: ajax('http://api.bart.gov/api/route.aspx?cmd=routeinfo&route=all&key=MW9S-E7SL-26DU-VV8V')
+        .then(function (response) {
+          var json = xmlToJson(response);
+          var routes = json.root.routes.route;
+
+          Ember.Logger.log(json);
+
+          return routes;
         })
       });
   }
