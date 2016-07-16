39d3e74bead8aee58cf90adcc8652dc67b4d4913
Lex Lacson <lex.lacson.dev@gmail.com>
2016-07-16 15:40:08 -0700

    we don't need stub data anymore since I hooked up real data

--- app/components/train-list/component.js
+++ app/components/train-list/component.js
@@ -3,29 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['train-list'],
 
-  availableTrains: [{
-    title: 'route A',
-    departureTime: '5:26am',
-    estimatedTime: '28min'
-  },
-  {
-    title: 'route B',
-    departureTime: '5:27am',
-    estimatedTime: '28min'
-  },
-  {
-    title: 'route B',
-    departureTime: '5:28am',
-    estimatedTime: '28min'
-  },
-  {
-    title: 'route B',
-    departureTime: '5:29am',
-    estimatedTime: '28min'
-  },
-  {
-    title: 'route B',
-    departureTime: '5:30am',
-    estimatedTime: '28min'
-  }]
+  availableTrains: []
 });
