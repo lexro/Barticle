876537b295a49781b5348f81b365653009e8a30a
Lex Lacson <lex.lacson.dev@gmail.com>
2016-07-24 12:03:05 -0700

    Update 2016-07-24T19:03:02.475Z

--- sw.js
+++ sw.js
@@ -1,6 +1,67 @@
+/* global caches */
+
 // urlsToCache will be modified by a post build script
-var urlsToCache = ['/Barticle/assets/barticle-0bc42cf76cc754eb84ad8f7149403272.css', '/Barticle/assets/barticle-c7d3e99b4e27426156b5a2be8042e934.js', '/Barticle/assets/vendor-5ea4b43c5bdd2261bcc3f6937cce2ecc.css', '/Barticle/assets/vendor-8ef3d164c76f6bc2c2df6243f5768c59.js', '/Barticle/crossdomain.xml', '/Barticle/index.html', '/Barticle'];
+var urlsToCache = ['/Barticle/assets/barticle-0bc42cf76cc754eb84ad8f7149403272.css', '/Barticle/assets/barticle-0eb36133c5bbccc6b56cff73bf052bad.js', '/Barticle/assets/vendor-469ae94e605e88fc1f8a1ea008cb0c59.js', '/Barticle/assets/vendor-fdf65018cf42c9b78fabee913b19426d.css', '/Barticle/crossdomain.xml', '/Barticle/index.html', '/Barticle'];
+var CACHE_NAME = 'barticle-cache-v1';
+
+this.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+this.addEventListener('activate', function (event) {
+  console.log('activate');
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          return cacheName.startWith('barticle-') && cacheName !== CACHE_NAME;
+        })
+        .map(function (cacheName) {
+          console.log('delete:', cacheName);
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
 
 this.addEventListener('fetch', function (event) {
-  console.log('hi hi hi:', event, urlsToCache);
+  console.log('fetch:', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          console.log('hit');
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function (response) {
+            if (!_shouldCache) {
+              console.log('should not cache');
+              return response;
+            }
+            console.log('open cache to cache');
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                console.log('cache response:', event.request.url);
+                cache.put(event.request, response.clone());
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
+
+function _shouldCache (response, url) {
+  var isBart = url.indexOf('https://api.bart.gov') !== -1;
+  var isLiveReload = url.indexOf('livereload') !== -1;
+  return  !isBart && !isLiveReload && !response || response.status !== 200 || response.type !== 'basic';
+}
