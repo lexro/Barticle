2c969bf41380162e33a14f2ec1f98eb0b552b149
Lex Lacson <lex.lacson.dev@gmail.com>
2016-07-25 21:55:19 -0700

    precache, fetch, and cache

--- app/service-worker/sw.js
+++ app/service-worker/sw.js
@@ -1,6 +1,68 @@
-// urlsToCache will be modified by a post build script
-var urlsToCache = [];
+/* global caches */
 
-this.addEventListener('fetch', function (event) {
-  console.log('hi hi hi:', event, urlsToCache);
+// urlsToCache will be modified by a post build script
+var urlsToCache = ['/assets/barticle.css', '/assets/barticle.css.map', '/assets/barticle.js', '/assets/barticle.map', '/assets/failed.png', '/assets/passed.png', '/assets/vendor.css', '/assets/vendor.js', '/assets/vendor.map', '/crossdomain.xml', '/index.html', '/'];
+var thirdPartyUrlsToCache = ['https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V', 'https://api.bart.gov/api/route.aspx?cmd=routeinfo&route=all&key=MW9S-E7SL-26DU-VV8V', 'https://fonts.googleapis.com/css?family=Roboto:400'];
+var CACHE_NAME = 'barticle-cache-v1';
+
+// precache this stuff
+this.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        var urls = urlsToCache.concat(thirdPartyUrlsToCache);
+        return cache.addAll(urls);
+      })
+  );
 });
+
+// delete other barticle caches
+this.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          return cacheName.startsWith('barticle-') && cacheName !== CACHE_NAME;
+        })
+        .map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// hijack requests and cache them
+this.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function (response) {
+            if (!_shouldCache) {
+              return response;
+            }
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, response.clone());
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+// cache bart responses
+function _shouldCache (response, url) {
+  var isBart = url.indexOf('https://api.bart.gov') !== -1;
+  var isLiveReload = url.indexOf('livereload') !== -1;
+
+  return  !isBart && !isLiveReload && !response || response.status !== 200 || response.type !== 'basic';
+}
