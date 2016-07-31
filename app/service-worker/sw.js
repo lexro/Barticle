d9cba05240174b7abf183617cd8a706d8fc5c190
Lex Lacson <lex.lacson.dev@gmail.com>
2016-07-30 19:37:40 -0700

    new cache

--- app/service-worker/sw.js
+++ app/service-worker/sw.js
@@ -4,7 +4,7 @@
 // urlsToCache will be modified by a post build script
 var urlsToCache = ['/assets/barticle.css', '/assets/barticle.css.map', '/assets/barticle.js', '/assets/barticle.map', '/assets/failed.png', '/assets/passed.png', '/assets/vendor.css', '/assets/vendor.js', '/assets/vendor.map', '/crossdomain.xml', '/index.html', '/'];
 var thirdPartyUrlsToCache = ['https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V', 'https://api.bart.gov/api/route.aspx?cmd=routeinfo&route=all&key=MW9S-E7SL-26DU-VV8V', 'https://fonts.googleapis.com/css?family=Roboto:400'];
-var CACHE_NAME = 'barticle-cache-v2';
+var CACHE_NAME = 'barticle-cache-v3';
 
 // precache this stuff
 this.addEventListener('install', function (event) {
