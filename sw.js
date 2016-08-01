7f39233e357ca0184e3047c068bdd5666f180931
Lex Lacson <lex.lacson.dev@gmail.com>
2016-07-31 22:39:17 -0700

    Update 2016-08-01T05:39:14.838Z

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // some stuff based on http://www.html5rocks.com/en/tutorials/service-worker/introduction/
 
 // urlsToCache will be modified by a post build script
-var urlsToCache = ['/Barticle/assets/barticle-53fdc8793ba32e824c05ddbd7cec86ca.css', '/Barticle/assets/barticle-ed8a17c038477bc4f808403f4b252997.js', '/Barticle/assets/vendor-61f7630a69b63b3d7351c2db45b5cf92.css', '/Barticle/assets/vendor-e8ea13569088fcaca55f275dd9ca3220.js', '/Barticle/crossdomain.xml', '/Barticle/images/foggy-golden-gate.jpeg', '/Barticle/index.html', '/Barticle'];
+var urlsToCache = ['/Barticle/assets/barticle-53fdc8793ba32e824c05ddbd7cec86ca.css', '/Barticle/assets/barticle-d1f0202ad5364d5005cc2caa427c62b5.js', '/Barticle/assets/vendor-61f7630a69b63b3d7351c2db45b5cf92.css', '/Barticle/assets/vendor-e8ea13569088fcaca55f275dd9ca3220.js', '/Barticle/crossdomain.xml', '/Barticle/images/foggy-golden-gate.jpeg', '/Barticle/index.html', '/Barticle'];
 var thirdPartyUrlsToCache = ['https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V', 'https://api.bart.gov/api/route.aspx?cmd=routeinfo&route=all&key=MW9S-E7SL-26DU-VV8V', 'https://fonts.googleapis.com/css?family=Roboto:400'];
 var CACHE_NAME = 'barticle-cache-v4';
 
